420bf0230591e82e5621134021c9c93d9eb3667c
avish950107 <avish950107@gmail.com>
2021-01-10 02:16:48 +0530

    asian guy commit

--- src/Hee.java
+++ src/Hee.java
@@ -7,6 +7,12 @@ System.out.println("hello this is asian guy avish");
 System.out.println("hello this is update coode");
 System.out.println("hello this is lasat commit");
 
+
+System.out.println("selienum test1");
+System.out.println("selienum test2");
+System.out.println("selienum test3");
+
+
 	}
 
 }

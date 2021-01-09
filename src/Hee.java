5f1841d3f5626f58f003b650370b9776628c591e
avish950107 <avish950107@gmail.com>
2021-01-10 01:58:28 +0530

    american time zone guy

--- src/Hee.java
+++ src/Hee.java
@@ -4,6 +4,9 @@ public class Hee {
 	public static void main(String[] args) {
 		
 System.out.println("hello this is asian guy avish");
+System.out.println("hello this is update coode");
+System.out.println("hello this is lasat commit");
+
 	}
 
 }

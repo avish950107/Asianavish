6cb820e23d2a44d39d1ef0df92a007ec2003da5f
avish950107 <avish950107@gmail.com>
2021-01-10 23:58:50 +0530

    architect asian development

--- src/Hee.java
+++ src/Hee.java
@@ -1,18 +1,25 @@
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 public class Hee {
-
-	public static void main(String[] args) {
-		
-System.out.println("hello this is asian guy avish");
-System.out.println("hello this is update coode");
-System.out.println("hello this is lasat commit");
-
-
-System.out.println("selienum test1");
-System.out.println("selienum test2");
-System.out.println("selienum test3");
+	@Test
+	public void website()
+	{
+		System.out.println("hello this is asian guy avish");
+		System.out.println("hello this is update coode");
+		System.out.println("hello this is lasat commit");
 
 
+		System.out.println("selienum test1");
+		System.out.println("selienum test2");
+		System.out.println("selienum test3");
 	}
-
+	@BeforeTest
+	public void Login()
+	{
+		System.out.println("credentials are--");
+		System.out.println("username are--");
+	}
 }
+
+
